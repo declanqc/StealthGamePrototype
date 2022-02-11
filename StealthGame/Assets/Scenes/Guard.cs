c08f5b18bb15a8e0c3f09f7f1e0cb7e7b35609e7
declanqc <72388492+declanqc@users.noreply.github.com>
2022-02-11 13:07:34 +0000

    added player character controller

--- StealthGame/Assets/Scenes/Guard.cs
+++ StealthGame/Assets/Scenes/Guard.cs
@@ -6,6 +6,7 @@ public class Guard : MonoBehaviour
 {
     public float speed = 5f;
     public float waitTime = .3f;
+    public float turnSpeed = 90f;
 
     public Transform pathHolder;
 
@@ -16,6 +17,7 @@ public class Guard : MonoBehaviour
         for(int i = 0; i < waypoints.Length; i ++)
         {
             waypoints [i] = pathHolder.GetChild(i).position;
+            waypoints[i] = new Vector3(waypoints[i].x, transform.position.y, waypoints[i].z);
         }
         StartCoroutine(FollowPath(waypoints));
     }
@@ -25,6 +27,7 @@ public class Guard : MonoBehaviour
         transform.position = waypoints [0];
         int targetWaypointIndex = 1;
         Vector3 targetWaypoint = waypoints[targetWaypointIndex];
+        transform.LookAt(targetWaypoint);
 
         while(true)
             {
@@ -34,7 +37,21 @@ public class Guard : MonoBehaviour
                 targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
                 targetWaypoint = waypoints[targetWaypointIndex];
                 yield return new WaitForSeconds(waitTime);
+                yield return StartCoroutine (TurnToFace(targetWaypoint));
             }
+            yield return null;
+        }
+    }
+
+    IEnumerator TurnToFace(Vector3 lookTarget)
+    {
+        Vector3 dirToLookTarget = (lookTarget - transform.position).normalized;
+        float targetAngle = 90 - Mathf.Atan2 (dirToLookTarget.z, dirToLookTarget.x) * Mathf.Rad2Deg;
+
+        while(Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle)) > 0.05f)
+        {
+            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnSpeed * Time.deltaTime);
+            transform.eulerAngles = Vector3.up * angle;
             yield return null;
         }
     }
