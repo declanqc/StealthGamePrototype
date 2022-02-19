29715593ba12adf95bf1e2336b42889b2daaf5ca
declanqc <72388492+declanqc@users.noreply.github.com>
2022-02-19 23:42:33 +0000

    added door and locker scripts

---
+++ StealthGame/Assets/Door.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Animator door;
+    bool NearDoor = false;
+    bool DoorOpen = false;
+
+     void Update()
+    {
+       
+        if (Input.GetKeyDown("e") && NearDoor)
+
+             {
+
+            DoorOpen = !DoorOpen;
+        
+             }
+
+        if(DoorOpen)
+        {
+            door.SetBool("NearDoor", true);
+        }
+        else if(!DoorOpen)
+        {
+            door.SetBool("NearDoor", false);
+        }
+        
+  
+
+  
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        
+
+        if(other.gameObject.CompareTag("Player"))
+        {
+            NearDoor = true;            
+            Debug.Log("neardoor");
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            NearDoor = false;
+            Debug.Log("notneardoor");
+        }
+    }
+}
---
+++ StealthGame/Assets/LockerHide.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockerHide : MonoBehaviour
+{
+    public GameObject player;
+    bool NearLocker = false;
+    bool InLocker = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            NearLocker = true;
+            Debug.Log("nearlocker");
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        NearLocker = false;
+    }
+    private void Update()
+    {
+        if(NearLocker == true && Input.GetKeyDown("e"))
+        {
+            player.transform.position = new Vector3(27.5f, 1f, 5.38000011f);
+            InLocker = !InLocker;
+        }
+
+        if(NearLocker && InLocker && Input.GetKeyDown("e"))
+        {
+            player.transform.position = new Vector3(27.5f, 1f, 2.70000005f);
+        }
+        else if(NearLocker && !InLocker && Input.GetKeyDown("e"))
+        {
+            player.transform.position = new Vector3(27.5f, 1f, 5.38000011f);
+        }
+
+    }
+
+
+}
